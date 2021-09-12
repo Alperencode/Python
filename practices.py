d2370d24fbdb9e90bf7cebd7174e23cd625425be
Alperen Ağa <alperencode@gmail.com>
2021-09-13 02:02:12 +0300

    Add practice.py files

---
+++ practices.py
@@ -0,0 +1,46 @@
+class Human:
+    def __init__(self,name:str,surname:str,age:int):
+        self.__name = name
+        self.__surname = surname 
+        self.__age = age 
+    
+    def getName(self):
+        return self.__name
+
+    def getSurname(self):
+        return self.__surname
+    
+    def getAge(self):
+        return str(self.__age)
+    
+    def changeName(self,newname):
+        self.__name = newname
+
+    def __str__(self):
+        return "Name: " + self.__name + " Surname: " + self.__surname + " Age: " + str(self.__age)
+    
+    def __len__(self):
+        return self.__age
+    
+    def __del__(self):
+        print("{} {} has deleted!".format(self.__name,self.__surname))
+
+class Worker(Human):
+    
+    def __init__(self,name:str,surname:str,age:int,idno:int,p_lang:tuple):
+        Human.__init__(self,name,surname,age)
+        self.__idno = idno
+        self.__p_lang = p_lang
+
+    def getIdno(self):
+        return self.__idno
+
+    def getP_lang(self):
+        return self.__p_lang
+
+x = Worker("Zeynep","Ağa",17,1,("Python","Php","Arduino"))
+print(x)
+x.getName()
+x.getP_lang()
+x.changeName("Alperen")
+x.getName()
---
+++ practices2.py
@@ -0,0 +1,85 @@
+class Insan:
+    def __init__(self,isim:str,soyisim:str,yas:int):
+        self.__isim = isim
+        self.__soyisim = soyisim
+        self.__yas = yas
+
+    def getIsim(self):
+        return self.__isim
+
+    def getSoyisim(self):
+        return self.__soyisim
+    
+    def getYas(self):
+        return str(self.__yas)
+
+    def changeIsim(self,isim:str):
+        if(type(isim) == "str"):
+            self.__isim = isim
+        else:
+            return "Please try with strings"
+    
+    def changeSoyisim(self,soyisim:str):
+        if(type(soyisim) == "str"):
+            self.__soyisim = soyisim
+        else:
+            return "Please try with strings"
+
+    def changeYas(self,yas:int):
+        if(type(yas) == "int"):
+            self.__yas = yas
+        else:
+            return "Please try with integers"
+
+    def __str__(self):
+        return "İsim: " + self.__isim + " Soyisim: " + self.__soyisim + " Yaş: " + self.__yas
+    
+    def __len__(self):
+        return self.__yas
+    
+    def __del__(self):
+        print("{} {} isimli kişi silindi! ".format(self.__isim, self.__soyisim))
+
+class Calismayan(Insan):
+    pass
+
+class Calisan(Insan):
+    def __init__(self,isim:str,soyisim:str,yas:int,isi:str,bilinen_diller:tuple):
+        Insan.__init__(self,isim,soyisim,yas)
+        self.__isim = isim    
+        self.__isi = isi
+        self.__bilinen_diller = bilinen_diller
+
+    def getIs(self):
+        return self.__isi
+
+    def getDiller(self):
+        return self.__bilinen_diller
+
+    def changeIs(self,yenis:str):
+        if(type(yenis) == "str"):
+            self.__isi = yenis
+        else:
+            return "Please try with strings"
+
+    def changeDiller(self,diller:tuple):
+        if(type(diller) == "tuple"):
+            self.__bilinen_diller = diller
+        else:
+            return "Please try with tuples"
+
+    def addDil(self,*args):
+        if(type(args) == tuple):
+            for x in args:
+                self.__bilinen_diller += x  
+        else:
+            return "Please try with tuples"      
+
+    def __str__(self):
+        return self.__isim + " İsimli kişi " + self.__isi + " işinde çalışıyor."
+
+    def __del__(self):
+        print(self.__isi,"işinde çalışan",self.__isim,"isimli kişi silindi!")
+
+x = Calisan("Alperen","Ağa",17,"Freelance Developer",("Python","Php","Arduino"))
+y = Calisan("Alperen","Ağa",17,"Freelance Developer",("Python","Php","Arduino"))
