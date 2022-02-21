d64289ad4c3fd0c1c2bbb26873824c3c273f487f
Alperen Aga <alperencode@gmail.com>
2022-02-21 21:14:02 +0300

    Update Magic-Methods

--- Advanced/Magic-Methods/main.py
+++ Advanced/Magic-Methods/main.py
@@ -1,1 +1,26 @@
-## Will be updated soon.
+# Dunders and Magic Methods
+class Vector:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def __add__(self,other):
+        return Vector(self.x + other.x, self.y + other.y)
+    
+    def __repr__(self):
+        return f"X: {self.x}, Y: {self.y}"
+
+    # manupilating length
+    def __len__(self):
+        return 10
+
+    def __call__(self):
+        print("Called Vector")
+
+v1 = Vector(20, 30)
+v2 = Vector(40, 50)
+v3 = v1 + v2
+
+print(len(v3))
+print(v3)
+v3()
