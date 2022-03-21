7aed1ef6d985c83dc346778606eb4247c23c9035
Alperen Aga <alperencode@gmail.com>
2022-03-22 01:48:15 +0300

    Update Sets (Will update again)

--- Advanced/Sets/main.py
+++ Advanced/Sets/main.py
@@ -1,1 +1,25 @@
-# I'll learn about sets in this file as soon as I can.
+# Sets
+# They are the basics of sets I'll dive into them later.
+
+# Set is a collection data type that is
+# - iterable
+# - mutable
+# - has no duplicate  elements
+
+numbers = [1,1,14,3,5,5]
+first = set(numbers)
+second = {1,2,3,4,5}
+
+# union
+print(first | second)
+
+# intersection
+print(first & second)
+
+# difference
+print(first - second)
+
+# symmetric difference
+print(first ^ second)
+
+# Sets does not support indexing
